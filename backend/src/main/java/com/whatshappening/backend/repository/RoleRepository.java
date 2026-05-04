037719cc84526e03359fd3e89e3e7b476c14a64e
ngenziwingenzi <nshutihubert4@gmail.com>
2026-05-04 16:54:42 +0200

    Add user and Role repository interfaces

---
+++ backend/src/main/java/com/whatshappening/backend/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.whatshappening.backend.repository;
+
+import com.whatshappening.backend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findByName(String name);
+}
